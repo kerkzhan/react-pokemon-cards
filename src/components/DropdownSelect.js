032ffca73605ae0acbb14e0890b2e18d545161f8
Kerk Zhan <kerk@nightmorning.com>
2020-09-25 16:36:33 +0800

    got http working

--- src/components/DropdownSelect.js
+++ src/components/DropdownSelect.js
@@ -2,13 +2,15 @@ import React from "react";
 import { useHttp } from "../hooks/http.js";
 
 const DropdownSelect = () => {
-    const fetchedData = useHttp('https://pokeapi.co/api/v2/pokemon?limit=5', []);
-    const selectedPokemon = fetchedData ? "good" : "bad";
-    console.log(fetchedData);
+    const fetchedData = useHttp('https://pokeapi.co/api/v2/pokemon?limit=9', []);
+    const selectedPokemon = fetchedData.includes(null) ? [] : fetchedData[0].results.map((pokemon, index) => ({
+        name: pokemon.name,
+        id: index+1
+    }));
     console.log(selectedPokemon);
     return (
         <div className="flex text-black">
-            <h2>selectedPokemon </h2>
+            <h2>selectedPokemon</h2>
         </div>
     )
 };
